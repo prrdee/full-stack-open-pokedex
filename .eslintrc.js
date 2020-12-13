58250d65c73dae4ba341e5aaff05aba118680815
prrdee <18732242+prrdee@users.noreply.github.com>
2020-12-13 21:07:54 +0200

    fix eslint config properly without ignoring test folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
-    "commonjs": true,
+    "browser": true,
+    "node": true,
     "es6": true,
-    "node": true
+    "jest/globals": true
   },
   "extends": [
     "eslint:recommended",
